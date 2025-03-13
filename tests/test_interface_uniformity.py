008d2240d764efc9e46ae01bbcb6cdc8e8a6bb6f
Sha <wangsha@users.noreply.github.com>
2025-03-13 13:36:05 +0800

    use default service port

--- tests/test_interface_uniformity.py
+++ tests/test_interface_uniformity.py
@@ -14,7 +14,7 @@ from cachelib import SimpleCache
 def create_cache_list(request, tmpdir):
     mc = MemcachedCache()
     mc._client.flush_all()
-    rc = RedisCache(port=6360)
+    rc = RedisCache()
     rc._write_client.flushdb()
     request.cls.cache_list = [FileSystemCache(tmpdir), mc, rc, SimpleCache()]
 
